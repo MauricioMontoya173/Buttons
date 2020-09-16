815fac8d9b1e03ca391530ac1925136aa7681aaf
DevMauricioMontoya <mauriciomontoya921@gmail.com>
2020-09-16 00:11:05 -0600

    The button is done

--- Form1.cs
+++ Form1.cs
@@ -19,7 +19,7 @@ namespace MiApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("","",MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show("Hola","En espanol",MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
