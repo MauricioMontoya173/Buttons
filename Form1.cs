7135f59c2d15a6aef408dc30ade26084b9a8b035
DevMauricioMontoya <mauriciomontoya921@gmail.com>
2020-09-15 23:50:34 -0600

    The button is ok

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace MiApp
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("","",MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
     }
 }
